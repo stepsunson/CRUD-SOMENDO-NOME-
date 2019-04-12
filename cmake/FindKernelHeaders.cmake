d303eb136caa8144dc22f989020df77145897dbc
stepsunson <104303353+stepsunson@users.noreply.github.com>
2019-04-12 22:54:00 +0000

    Create constants extension

--- cmake/FindKernelHeaders.cmake
+++ cmake/FindKernelHeaders.cmake
@@ -22,4 +22,14 @@ message(STATUS "Kernel headers: ${KERNELHEADERS_DIR}")
 
 if (KERNELHEADERS_DIR)
     set(KERNELHEADERS_INCLUDE_DIRS
-    
+            ${KERNELHEADERS_DIR}/include/generated/uapi
+            CACHE PATH "Kernel headers include dirs"
+            )
+    set(KERNELHEADERS_FOUND 1 CACHE STRING "Set to 1 if kernel headers were found")
+    include_directories(${KERNELHEADERS_INCLUDE_DIRS})
+else (KERNELHEADERS_DIR)
+    set(KERNELHEADERS_FOUND 0 CACHE STRING "Set to 1 if kernel headers were found")
+endif (KERNELHEADERS_DIR)
+
+mark_as_advanced(KERNELHEADERS_FOUND)
+
