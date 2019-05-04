9d221259df4e04d7b9c42b962f5960d52284c019
stepsunson <104303353+stepsunson@users.noreply.github.com>
2019-05-04 09:57:32 +0000

    remove a file

---
+++ examples/lua/kprobe-write.lua
@@ -0,0 +1,10 @@
+#!/usr/bin/env bcc-lua
+--[[
+Copyright 2016 Marek Vavrusa <mvavrusa@cloudflare.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
