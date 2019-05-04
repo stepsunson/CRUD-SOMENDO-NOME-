57546f58db2d0df33b60edb1e539aaf8bc3c85da
stepsunson <104303353+stepsunson@users.noreply.github.com>
2019-05-04 07:56:32 +0000

    mm, vmalloc: remove useless variable in vmap_block

--- examples/lua/bashreadline.c
+++ examples/lua/bashreadline.c
@@ -7,4 +7,16 @@ struct str_t {
 
 BPF_PERF_OUTPUT(events);
 
-int printret(struct pt_regs *ct
+int printret(struct pt_regs *ctx)
+{
+	struct str_t data  = {};
+	u32 pid;
+        if (!PT_REGS_RC(ctx))
+          return 0;
+        pid = bpf_get_current_pid_tgid();
+        data.pid = pid;
+        bpf_probe_read_user(&data.str, sizeof(data.str),
+                            (void *)PT_REGS_RC(ctx));
+        events.perf_submit(ctx, &data, sizeof(data));
+        return 0;
+};
