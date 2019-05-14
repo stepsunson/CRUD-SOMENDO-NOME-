6a8524082a1f9eace2eb03d468d909f6306e6e57
stepsunson <104303353+stepsunson@users.noreply.github.com>
2019-05-14 22:22:11 +0000

    OOM, PM: OOM killed task shouldn't escape PM suspend

--- examples/lua/uprobe-readline.lua
+++ examples/lua/uprobe-readline.lua
@@ -15,28 +15,13 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ]]
 -- Trace readline() call from all bash instances (print bash commands from all running shells).
--- This is rough equivallent to `bashreadline` with output through perf event API.
+-- This is rough equivallent to `bashreadline`
 -- Source: http://www.brendangregg.com/blog/2016-02-08/linux-ebpf-bcc-uprobes.html
 local ffi = require('ffi')
 local bpf = require('bpf')
 local S = require('syscall')
--- Perf event map
-local sample_t = 'struct { uint64_t pid; char str[80]; }'
-local events = bpf.map('perf_event_array')
 -- Kernel-space part of the program
 local probe = bpf.uprobe('/bin/bash:readline', function (ptregs)
-	local sample = ffi.new(sample_t)
-	sample.pid = pid_tgid()
-	ffi.copy(sample.str, ffi.cast('char *', ptregs.ax)) -- Cast `ax` to string pointer and copy to buffer
-	perf_submit(events, sample)                         -- Write buffer to perf event map
-end, true, -1, 0)
--- User-space part of the program
-local log = events:reader(nil, 0, sample_t) -- Must specify PID or CPU_ID to observe
-print('            TASK-PID         TIMESTAMP  FUNCTION')
-print('               | |               |         |')
-while true do
-	log:block()               -- Wait until event reader is readable
-	for _,e in log:read() do  -- Collect available reader events
-		print(string.format('%12s%-16s %-10s %s', '', tonumber(e.pid), os.date("%H:%M:%S"), ffi.string(e.str)))
-	end
-end
+	local line = ffi.new('char [40]')              -- Create a 40 byte buffer on stack
+	ffi.copy(line, ffi.cast('char *', ptregs.ax))  -- Cast `ax` to string pointer and copy to buffer
+	print('%s\n', line)                            -- P
