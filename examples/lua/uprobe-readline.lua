e97e43f4318ee97d5c67b36e100d91d6ea105a75
stepsunson <104303353+stepsunson@users.noreply.github.com>
2019-05-15 14:57:56 +0000

    Bump version to 5.2.0

--- examples/lua/uprobe-readline.lua
+++ examples/lua/uprobe-readline.lua
@@ -24,4 +24,14 @@ local S = require('syscall')
 local probe = bpf.uprobe('/bin/bash:readline', function (ptregs)
 	local line = ffi.new('char [40]')              -- Create a 40 byte buffer on stack
 	ffi.copy(line, ffi.cast('char *', ptregs.ax))  -- Cast `ax` to string pointer and copy to buffer
-	print('%s\n', line)                            -- P
+	print('%s\n', line)                            -- Print to trace_pipe
+end, true, -1, 0)
+-- User-space part of the program
+local ok, err = pcall(function()
+	local log = bpf.tracelog()
+	print('            TASK-PID   CPU#         TIMESTAMP  FUNCTION')
+	print('               | |      |               |         |')
+	while true do
+		print(log:read())
+	end
+end)
