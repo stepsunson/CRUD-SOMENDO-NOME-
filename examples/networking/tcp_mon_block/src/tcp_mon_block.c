a3bda5046c4e531ced093a44466d1d7bd435ee39
stepsunson <104303353+stepsunson@users.noreply.github.com>
2019-06-03 09:16:52 +0000

    Update release.md

--- examples/networking/tcp_mon_block/src/tcp_mon_block.c
+++ examples/networking/tcp_mon_block/src/tcp_mon_block.c
@@ -97,3 +97,84 @@ int handle_egress(struct __sk_buff *ctx)
     void* data_end = (void*)(long)ctx->data_end;
     void* data = (void*)(long)ctx->data;
     struct ethhdr *eth = data;
+    struct iphdr *ip = data + sizeof(*eth);
+    struct tcphdr *tcp;
+    key_hash key = {};
+
+    /* length check */
+    if (data + sizeof(*eth) + sizeof(*ip) > data_end)
+    {
+        return TC_ACT_OK;
+    }
+
+    if (eth->h_proto != htons(ETH_P_IP))
+    {
+        return TC_ACT_OK;
+    }
+
+    if (ip->protocol != IPPROTO_TCP)
+    {
+        return TC_ACT_OK;
+    }
+
+    tcp = (void *)ip + sizeof(*ip);
+    if (tcp_header_bound_check(tcp, data_end))
+    {
+        return TC_ACT_OK;
+    }
+
+    u8 tcpflags = ((u_int8_t *)tcp)[13];
+    u16 src_port = bpf_ntohs(tcp->source);
+    u16 dst_port = bpf_ntohs(tcp->dest);
+
+    key.src_ip = ip->saddr;
+    key.src_port = src_port;
+    key.dst_ip = ip->daddr;
+    key.dst_port = dst_port;
+
+    full_packet *packet_value;
+    packet_value = monitored_connections.lookup(&key);
+    if (packet_value != 0)
+    {
+        packet_value->tcp_flags = tcpflags;
+        blocked_events.perf_submit(ctx, packet_value, sizeof(full_packet));
+        return TC_ACT_SHOT;
+    }
+
+    return TC_ACT_OK;
+}
+
+
+// Removing the entry from monitored_connections when the socket closes after failed connection
+TRACEPOINT_PROBE(sock, inet_sock_set_state)
+{
+    if (args->protocol != IPPROTO_TCP)
+    {
+        return 0;
+    }
+
+    if (args->newstate != TCP_CLOSE && args->newstate != TCP_CLOSE_WAIT)
+    {
+        return 0;
+    }
+
+    if (args->family == AF_INET)
+    {
+        key_hash key = {};
+        struct sock *sk = (struct sock *)args->skaddr;
+
+        key.src_port = args->sport;
+        key.dst_port = args->dport;
+        __builtin_memcpy(&key.src_ip, args->saddr, sizeof(key.src_ip));
+        __builtin_memcpy(&key.dst_ip, args->daddr, sizeof(key.dst_ip));
+
+        full_packet *packet_value;
+        packet_value = monitored_connections.lookup(&key);
+        if (packet_value != 0)
+        {
+            monitored_connections.delete(&key);
+            if (VERBOSE_OUTPUT)
+            {
+                verbose_event v = {};
+                make_verbose_event(&v, packet_value->src_ip, packet_value->dst_ip, packet_value->src_port, packet_value->dst_port, packet_value->pid, 3);
+                verbose_events.perf_submit(args, &v, sizeof(
