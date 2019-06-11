5cbd1ad26c51d31cd1872e97027cfd42ca448432
stepsunson <104303353+stepsunson@users.noreply.github.com>
2019-06-11 12:40:40 +0000

    mmc: Add new API call 'sdio_reset_comm' for resetting communication with an SDIO device

--- examples/networking/tunnel_monitor/traffic.sh
+++ examples/networking/tunnel_monitor/traffic.sh
@@ -18,4 +18,26 @@ if [[ $? != 0 ]]; then
   tmux new-window -t $S -n "c3"
   tmux send -t $S "$cmd ping 192.168.0.3 -s1024" C-m
   tmux new-window -t $S -n "c3"
-  tmux sen
+  tmux send -t $S "$cmd ping 192.168.0.4 -s128" C-m
+  tmux new-window -t $S -n "c3"
+  tmux send -t $S "$cmd ping 192.168.0.5 -s128" C-m
+  tmux new-window -t $S -n "c3"
+  tmux send -t $S "$cmd ping 192.168.0.6 -s128" C-m
+  tmux new-window -t $S -n "c4"
+  tmux send -t $S "$cmd ping 192.168.1.2 -s128" C-m
+  tmux new-window -t $S -n "c5"
+  tmux send -t $S "$cmd ping 192.168.1.4 -s768" C-m
+  tmux new-window -t $S -n "c2"
+  tmux send -t $S "$cmd ping 192.168.2.2 -s128" C-m
+  tmux new-window -t $S -n "c3"
+  tmux send -t $S "$cmd ping 192.168.2.7 -s1024" C-m
+  tmux new-window -t $S -n "c4"
+  tmux send -t $S "$cmd ping 192.168.2.2 -s128" C-m
+  tmux new-window -t $S -n "c5"
+  tmux send -t $S "$cmd ping 192.168.3.8 -s768" C-m
+  tmux new-window -t $S -n "c5"
+  tmux send -t $S "$cmd ping 192.168.3.9 -s768" C-m
+fi
+
+exec tmux attach -t $S
+
