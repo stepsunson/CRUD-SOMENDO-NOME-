c2fdd4b7bb8685b8dcc69b70b268f162b5f1933c
stepsunson <104303353+stepsunson@users.noreply.github.com>
2019-06-27 22:50:45 +0000

    :D

---
+++ examples/tracing/strlen_hist.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+#
+# strlen_hist.py   Histogram of system-wide strlen return values
+#
+# A basic example of using uprobes along with a histogram to show
+# distributions.
+#
+# Runs until ctrl-c is pressed.
+#
+# Copyright (c) PLUMgrid, Inc.
+# Licensed under the Apache License, Version 2.0 (the "License")
+#
+# Example output:
+# $ 
