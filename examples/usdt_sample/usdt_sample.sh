6f376deb8cd7444566f7f4d9a70e3a50e0488874
stepsunson <104303353+stepsunson@users.noreply.github.com>
2019-07-22 09:39:44 +0000

    drm/nouveau: fix init with agpgart-uninorth

--- examples/usdt_sample/usdt_sample.sh
+++ examples/usdt_sample/usdt_sample.sh
@@ -40,4 +40,52 @@ sudo pkill -f "\\-p.${pid}"
 sudo python3 examples/usdt_sample/scripts/lat_dist.py -p=${pid} -i=5 -f="usdt_20" &
 sleep 30
 sudo pkill -f "\\-p.${pid}"
-sudo python3 examples/usdt_sample/scripts/lat_dist.py -p=${p
+sudo python3 examples/usdt_sample/scripts/lat_dist.py -p=${pid} -i=5 -f="usdt_20" -s &
+sleep 30
+sudo pkill -f "\\-p.${pid}"
+sudo python3 examples/usdt_sample/scripts/lat_avg.py -p=${pid} -i=5 -c=10 -f="usdt_20" &
+sleep 30
+sudo pkill -f "\\-p.${pid}"
+sudo python3 examples/usdt_sample/scripts/lat_avg.py -p=${pid} -i=5 -c=10 -f="usdt_20" -s &
+sleep 30
+sudo pkill -f "\\-p.${pid}"
+
+sudo pkill -f "examples/usdt_sample/build_.*/usdt_sample_app1/usdt_sample_app1"
+
+clang --version
+rm -rf examples/usdt_sample/build_clang
+mkdir -p examples/usdt_sample/build_clang && pushd examples/usdt_sample/build_clang
+cmake .. -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
+make
+popd
+
+python3 tools/tplist.py -l examples/usdt_sample/build_clang/usdt_sample_lib1/libusdt_sample_lib1.so
+readelf -n examples/usdt_sample/build_clang/usdt_sample_lib1/libusdt_sample_lib1.so
+
+examples/usdt_sample/build_clang/usdt_sample_app1/usdt_sample_app1 "usdt" 1 30 10 1 50 &
+pid=$!
+
+echo "argdist.py - Using non-sdt probes"
+sudo python3 tools/argdist.py -p ${pid} -i 5 -C "u:$(pwd)/examples/usdt_sample/build_clang/usdt_sample_lib1/libusdt_sample_lib1.so:operation_start():char*:arg2#input" -z 32 &
+sleep 30
+sudo pkill -f "\\-p.${pid}"
+echo "argdist.py - Using sdt probes"
+sudo python3 tools/argdist.py -p ${pid} -i 5 -C "u:$(pwd)/examples/usdt_sample/build_clang/usdt_sample_lib1/libusdt_sample_lib1.so:operation_start_sdt():char*:arg2#input" -z 32 &
+sleep 30
+sudo pkill -f "\\-p.${pid}"
+sudo python3 examples/usdt_sample/scripts/latency.py -p=${pid} -f="usdt_20" &
+sleep 30
+sudo pkill -f "\\-p.${pid}"
+sudo python3 examples/usdt_sample/scripts/latency.py -p=${pid} -f="usdt_20" -s &
+sleep 30
+sudo pkill -f "\\-p.${pid}"
+sudo python3 examples/usdt_sample/scripts/lat_dist.py -p=${pid} -i=5 -f="usdt_20" &
+sleep 30
+sudo pkill -f "\\-p.${pid}"
+sudo python3 examples/usdt_sample/scripts/lat_dist.py -p=${pid} -i=5 -f="usdt_20" -s &
+sleep 30
+sudo pkill -f "\\-p.${pid}"
+sudo python3 examples/usdt_sample/scripts/lat_avg.py -p=${pid} -i=5 -c=10 -f="usdt_20" &
+sleep 30
+sudo pkill -f "\\-p.${pid}"
+sudo python3 examples/usdt_sample/scripts/l
