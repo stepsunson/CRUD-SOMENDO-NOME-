64caf915d1a744e3e02f587e145d9db6db97a4f0
stepsunson <104303353+stepsunson@users.noreply.github.com>
2019-11-04 20:54:24 +0000

    Unindent

--- libbpf-tools/biopattern.c
+++ libbpf-tools/biopattern.c
@@ -34,4 +34,90 @@ const char *argp_program_bug_address =
 const char argp_program_doc[] =
 "Show block device I/O pattern.\n"
 "\n"
-"USAGE: biopat
+"USAGE: biopattern [--help] [-T] [-d DISK] [interval] [count]\n"
+"\n"
+"EXAMPLES:\n"
+"    biopattern              # show block I/O pattern\n"
+"    biopattern 1 10         # print 1 second summaries, 10 times\n"
+"    biopattern -T 1         # 1s summaries with timestamps\n"
+"    biopattern -d sdc       # trace sdc only\n";
+
+static const struct argp_option opts[] = {
+	{ "timestamp", 'T', NULL, 0, "Include timestamp on output" },
+	{ "disk",  'd', "DISK",  0, "Trace this disk only" },
+	{ "verbose", 'v', NULL, 0, "Verbose debug output" },
+	{ NULL, 'h', NULL, OPTION_HIDDEN, "Show the full help" },
+	{},
+};
+
+static error_t parse_arg(int key, char *arg, struct argp_state *state)
+{
+	static int pos_args;
+
+	switch (key) {
+	case 'h':
+		argp_state_help(state, stderr, ARGP_HELP_STD_HELP);
+		break;
+	case 'v':
+		env.verbose = true;
+		break;
+	case 'd':
+		env.disk = arg;
+		if (strlen(arg) + 1 > DISK_NAME_LEN) {
+			fprintf(stderr, "invaild disk name: too long\n");
+			argp_usage(state);
+		}
+		break;
+	case 'T':
+		env.timestamp = true;
+		break;
+	case ARGP_KEY_ARG:
+		errno = 0;
+		if (pos_args == 0) {
+			env.interval = strtol(arg, NULL, 10);
+			if (errno) {
+				fprintf(stderr, "invalid internal\n");
+				argp_usage(state);
+			}
+		} else if (pos_args == 1) {
+			env.times = strtol(arg, NULL, 10);
+			if (errno) {
+				fprintf(stderr, "invalid times\n");
+				argp_usage(state);
+			}
+		} else {
+			fprintf(stderr,
+				"unrecognized positional argument: %s\n", arg);
+			argp_usage(state);
+		}
+		pos_args++;
+		break;
+	default:
+		return ARGP_ERR_UNKNOWN;
+	}
+	return 0;
+}
+
+static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va_list args)
+{
+	if (level == LIBBPF_DEBUG && !env.verbose)
+		return 0;
+	return vfprintf(stderr, format, args);
+}
+
+static void sig_handler(int sig)
+{
+	exiting = true;
+}
+
+static int print_map(struct bpf_map *counters, struct partitions *partitions)
+{
+	__u32 total, lookup_key = -1, next_key;
+	int err, fd = bpf_map__fd(counters);
+	const struct partition *partition;
+	struct counter counter;
+	struct tm *tm;
+	char ts[32];
+	time_t t;
+
+	while (!bpf_map_get_next_key(fd, &
