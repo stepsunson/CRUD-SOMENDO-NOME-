0fbe4d7d5d19b9f87f31d8694f1ac87ea7aceec1
stepsunson <104303353+stepsunson@users.noreply.github.com>
2019-12-18 22:09:42 +0000

    wahay

--- libbpf-tools/cachestat.c
+++ libbpf-tools/cachestat.c
@@ -90,4 +90,91 @@ static error_t parse_arg(int key, char *arg, struct argp_state *state)
 
 static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va_list args)
 {
-	if (level == LIBBPF_DEBUG &&
+	if (level == LIBBPF_DEBUG && !env.verbose)
+		return 0;
+	return vfprintf(stderr, format, args);
+}
+
+static void sig_handler(int sig)
+{
+	exiting = true;
+}
+
+static int get_meminfo(__u64 *buffers, __u64 *cached)
+{
+	FILE *f;
+
+	f = fopen("/proc/meminfo", "r");
+	if (!f)
+		return -1;
+	if (fscanf(f,
+		   "MemTotal: %*u kB\n"
+		   "MemFree: %*u kB\n"
+		   "MemAvailable: %*u kB\n"
+		   "Buffers: %llu kB\n"
+		   "Cached: %llu kB\n",
+		   buffers, cached) != 2) {
+		fclose(f);
+		return -1;
+	}
+	fclose(f);
+	return 0;
+}
+
+int main(int argc, char **argv)
+{
+	static const struct argp argp = {
+		.options = opts,
+		.parser = parse_arg,
+		.doc = argp_program_doc,
+	};
+	__u64 buffers, cached, mbd;
+	struct cachestat_bpf *obj;
+	__s64 total, misses, hits;
+	struct tm *tm;
+	float ratio;
+	char ts[32];
+	time_t t;
+	int err;
+
+	err = argp_parse(&argp, argc, argv, 0, NULL, NULL);
+	if (err)
+		return err;
+
+	libbpf_set_strict_mode(LIBBPF_STRICT_ALL);
+	libbpf_set_print(libbpf_print_fn);
+
+	obj = cachestat_bpf__open();
+	if (!obj) {
+		fprintf(stderr, "failed to open BPF object\n");
+		return 1;
+	}
+
+	/**
+	 * account_page_dirtied was renamed to folio_account_dirtied
+	 * in kernel commit 203a31516616 ("mm/writeback: Add __folio_mark_dirty()")
+	 */
+	if (fentry_can_attach("folio_account_dirtied", NULL)) {
+		err = bpf_program__set_attach_target(obj->progs.fentry_account_page_dirtied, 0,
+						     "folio_account_dirtied");
+		if (err) {
+			fprintf(stderr, "failed to set attach target\n");
+			goto cleanup;
+		}
+	}
+	if (kprobe_exists("folio_account_dirtied")) {
+		bpf_program__set_autoload(obj->progs.kprobe_account_page_dirtied, false);
+	} else {
+		bpf_program__set_autoload(obj->progs.kprobe_folio_account_dirtied, false);
+	}
+
+	/* It fallbacks to kprobes when kernel does not support fentry. */
+	if (fentry_can_attach("folio_account_dirtied", NULL)
+		|| fentry_can_attach("account_page_dirtied", NULL)) {
+		bpf_program__set_autoload(obj->progs.kprobe_account_page_dirtied, false);
+	} else {
+		bpf_program__set_autoload(obj->progs.fentry_account_page_dirtied, false);
+	}
+
+	if (fentry_can_attach("add_to_page_cache_lru", NULL)) {
+		bpf_program__set_autoload(obj->pro
