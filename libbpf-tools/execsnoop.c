f4d9aa0bae5385c85f305a8240e35c0b09fb80ae
stepsunson <104303353+stepsunson@users.noreply.github.com>
2020-02-24 18:52:23 +0000

    Update hiver_TV.html

--- libbpf-tools/execsnoop.c
+++ libbpf-tools/execsnoop.c
@@ -55,4 +55,75 @@ const char argp_program_doc[] =
 "   ./execsnoop           # trace all exec() syscalls\n"
 "   ./execsnoop -x        # include failed exec()s\n"
 "   ./execsnoop -T        # include time (HH:MM:SS)\n"
-"
+"   ./execsnoop -U        # include UID\n"
+"   ./execsnoop -u 1000   # only trace UID 1000\n"
+"   ./execsnoop -t        # include timestamps\n"
+"   ./execsnoop -q        # add \"quotemarks\" around arguments\n"
+"   ./execsnoop -n main   # only print command lines containing \"main\"\n"
+"   ./execsnoop -l tpkg   # only print command where arguments contains \"tpkg\""
+"   ./execsnoop -c CG     # Trace process under cgroupsPath CG\n";
+
+static const struct argp_option opts[] = {
+	{ "time", 'T', NULL, 0, "include time column on output (HH:MM:SS)" },
+	{ "timestamp", 't', NULL, 0, "include timestamp on output" },
+	{ "fails", 'x', NULL, 0, "include failed exec()s" },
+	{ "uid", 'u', "UID", 0, "trace this UID only" },
+	{ "quote", 'q', NULL, 0, "Add quotemarks (\") around arguments" },
+	{ "name", 'n', "NAME", 0, "only print commands matching this name, any arg" },
+	{ "line", 'l', "LINE", 0, "only print commands where arg contains this line" },
+	{ "print-uid", 'U', NULL, 0, "print UID column" },
+	{ "max-args", MAX_ARGS_KEY, "MAX_ARGS", 0,
+		"maximum number of arguments parsed and displayed, defaults to 20" },
+	{ "verbose", 'v', NULL, 0, "Verbose debug output" },
+	{ "cgroup", 'c', "/sys/fs/cgroup/unified", 0, "Trace process in cgroup path"},
+	{ NULL, 'h', NULL, OPTION_HIDDEN, "Show the full help" },
+	{},
+};
+
+static error_t parse_arg(int key, char *arg, struct argp_state *state)
+{
+	long int uid, max_args;
+
+	switch (key) {
+	case 'h':
+		argp_state_help(state, stderr, ARGP_HELP_STD_HELP);
+		break;
+	case 'T':
+		env.time = true;
+		break;
+	case 't':
+		env.timestamp = true;
+		break;
+	case 'x':
+		env.fails = true;
+		break;
+	case 'c':
+		env.cgroupspath = arg;
+		env.cg = true;
+		break;
+	case 'u':
+		errno = 0;
+		uid = strtol(arg, NULL, 10);
+		if (errno || uid < 0 || uid >= INVALID_UID) {
+			fprintf(stderr, "Invalid UID %s\n", arg);
+			argp_usage(state);
+		}
+		env.uid = uid;
+		break;
+	case 'q':
+		env.quote = true;
+		break;
+	case 'n':
+		env.name = arg;
+		break;
+	case 'l':
+		env.line = arg;
+		break;
+	case 'U':
+		env.print_uid = true;
+		break;
+	case 'v':
+		env.verbose = true;
+		break;
+	case MAX_ARGS_KEY:
+		e
