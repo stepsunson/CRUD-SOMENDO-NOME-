9f06afa69553886f8434c8c3059815b57b7d2f6a
stepsunson <104303353+stepsunson@users.noreply.github.com>
2020-04-28 15:51:49 +0000

    Pontoon: Update Kabyle (kab) localization of Thimble

--- libbpf-tools/fsslower.c
+++ libbpf-tools/fsslower.c
@@ -74,4 +74,80 @@ static char file_op[] = {
 	[F_READ] = 'R',
 	[F_WRITE] = 'W',
 	[F_OPEN] = 'O',
-	[F_FS
+	[F_FSYNC] = 'F',
+};
+
+static volatile sig_atomic_t exiting = 0;
+
+/* options */
+static enum fs_type fs_type = NONE;
+static pid_t target_pid = 0;
+static time_t duration = 0;
+static __u64 min_lat_ms = 10;
+static bool csv = false;
+static bool verbose = false;
+
+const char *argp_program_version = "fsslower 0.1";
+const char *argp_program_bug_address =
+	"https://github.com/iovisor/bcc/tree/master/libbpf-tools";
+const char argp_program_doc[] =
+"Trace file system operations slower than a threshold.\n"
+"\n"
+"Usage: fsslower [-h] [-t FS] [-p PID] [-m MIN] [-d DURATION] [-c]\n"
+"\n"
+"EXAMPLES:\n"
+"    fsslower -t ext4             # trace ext4 operations slower than 10 ms\n"
+"    fsslower -t nfs -p 1216      # trace nfs operations with PID 1216 only\n"
+"    fsslower -t xfs -c -d 1      # trace xfs operations for 1s with csv output\n";
+
+static const struct argp_option opts[] = {
+	{ "csv", 'c', NULL, 0, "Output as csv" },
+	{ "duration", 'd', "DURATION", 0, "Total duration of trace in seconds" },
+	{ "pid", 'p', "PID", 0, "Process ID to trace" },
+	{ "min", 'm', "MIN", 0, "Min latency to trace, in ms (default 10)" },
+	{ "type", 't', "Filesystem", 0, "Which filesystem to trace, [btrfs/ext4/nfs/xfs]" },
+	{ "verbose", 'v', NULL, 0, "Verbose debug output" },
+	{ NULL, 'h', NULL, OPTION_HIDDEN, "Show the full help" },
+	{},
+};
+
+static error_t parse_arg(int key, char *arg, struct argp_state *state)
+{
+	switch (key) {
+	case 'v':
+		verbose = true;
+		break;
+	case 'c':
+		csv = true;
+		break;
+	case 'd':
+		errno = 0;
+		duration = strtol(arg, NULL, 10);
+		if (errno || duration <= 0) {
+			warn("invalid DURATION: %s\n", arg);
+			argp_usage(state);
+		}
+		break;
+	case 'm':
+		errno = 0;
+		min_lat_ms = strtoll(arg, NULL, 10);
+		if (errno || min_lat_ms < 0) {
+			warn("invalid latency (in ms): %s\n", arg);
+		}
+		break;
+	case 't':
+		if (!strcmp(arg, "btrfs")) {
+			fs_type = BTRFS;
+		} else if (!strcmp(arg, "ext4")) {
+			fs_type = EXT4;
+		} else if (!strcmp(arg, "nfs")) {
+			fs_type = NFS;
+		} else if (!strcmp(arg, "xfs")) {
+			fs_type = XFS;
+		} else {
+			warn("invalid filesystem\n");
+			argp_usage(state);
+		}
+		break;
+	case 'p':
+		errn
