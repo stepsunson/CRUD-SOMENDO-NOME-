5b4520c8e9b71a7f5e23d1cbd693d50198613a17
stepsunson <104303353+stepsunson@users.noreply.github.com>
2021-04-05 14:03:03 +0000

    hg

--- libbpf-tools/tcpstates.c
+++ libbpf-tools/tcpstates.c
@@ -22,4 +22,70 @@
 #include "tcpstates.skel.h"
 #include "trace_helpers.h"
 
-#define PERF_BUFFE
+#define PERF_BUFFER_PAGES	16
+#define PERF_POLL_TIMEOUT_MS	100
+#define warn(...) fprintf(stderr, __VA_ARGS__)
+
+static volatile sig_atomic_t exiting = 0;
+
+static bool emit_timestamp = false;
+static short target_family = 0;
+static char *target_sports = NULL;
+static char *target_dports = NULL;
+static bool wide_output = false;
+static bool verbose = false;
+static const char *tcp_states[] = {
+	[1] = "ESTABLISHED",
+	[2] = "SYN_SENT",
+	[3] = "SYN_RECV",
+	[4] = "FIN_WAIT1",
+	[5] = "FIN_WAIT2",
+	[6] = "TIME_WAIT",
+	[7] = "CLOSE",
+	[8] = "CLOSE_WAIT",
+	[9] = "LAST_ACK",
+	[10] = "LISTEN",
+	[11] = "CLOSING",
+	[12] = "NEW_SYN_RECV",
+	[13] = "UNKNOWN",
+};
+
+const char *argp_program_version = "tcpstates 1.0";
+const char *argp_program_bug_address =
+	"https://github.com/iovisor/bcc/tree/master/libbpf-tools";
+const char argp_program_doc[] =
+"Trace TCP session state changes and durations.\n"
+"\n"
+"USAGE: tcpstates [-4] [-6] [-T] [-L lport] [-D dport]\n"
+"\n"
+"EXAMPLES:\n"
+"    tcpstates                  # trace all TCP state changes\n"
+"    tcpstates -T               # include timestamps\n"
+"    tcpstates -L 80            # only trace local port 80\n"
+"    tcpstates -D 80            # only trace remote port 80\n";
+
+static const struct argp_option opts[] = {
+	{ "verbose", 'v', NULL, 0, "Verbose debug output" },
+	{ "timestamp", 'T', NULL, 0, "Include timestamp on output" },
+	{ "ipv4", '4', NULL, 0, "Trace IPv4 family only" },
+	{ "ipv6", '6', NULL, 0, "Trace IPv6 family only" },
+	{ "wide", 'w', NULL, 0, "Wide column output (fits IPv6 addresses)" },
+	{ "localport", 'L', "LPORT", 0, "Comma-separated list of local ports to trace." },
+	{ "remoteport", 'D', "DPORT", 0, "Comma-separated list of remote ports to trace." },
+	{ NULL, 'h', NULL, OPTION_HIDDEN, "Show the full help" },
+	{},
+};
+
+static error_t parse_arg(int key, char *arg, struct argp_state *state)
+{
+	long port_num;
+	char *port;
+
+	switch (key) {
+	case 'v':
+		verbose = true;
+		break;
+	case 'T':
+		emit_timestamp = true;
+		break;
+	case 
