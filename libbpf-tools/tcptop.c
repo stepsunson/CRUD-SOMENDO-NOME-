5bb8deff5a06b85a68b505af5b8071201150b60e
stepsunson <104303353+stepsunson@users.noreply.github.com>
2021-04-12 19:40:16 +0000

    sound: Fix make allmodconfig on MIPS

--- libbpf-tools/tcptop.c
+++ libbpf-tools/tcptop.c
@@ -2,4 +2,77 @@
 
 /*
  * tcptop Trace sending and received operation over IP.
- * Copyri
+ * Copyright (c) 2022 Francis Laniel <flaniel@linux.microsoft.com>
+ *
+ * Based on tcptop(8) from BCC by Brendan Gregg.
+ * 03-Mar-2022   Francis Laniel   Created this.
+ */
+#include <argp.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <unistd.h>
+
+#include <bpf/libbpf.h>
+#include <bpf/bpf.h>
+#include "tcptop.h"
+#include "tcptop.skel.h"
+#include "trace_helpers.h"
+
+#define warn(...) fprintf(stderr, __VA_ARGS__)
+#define OUTPUT_ROWS_LIMIT 10240
+
+#define IPV4 0
+#define PORT_LENGTH 5
+
+enum SORT {
+	ALL,
+	SENT,
+	RECEIVED,
+};
+
+static volatile sig_atomic_t exiting = 0;
+
+static pid_t target_pid = -1;
+static char *cgroup_path;
+static bool cgroup_filtering = false;
+static bool clear_screen = true;
+static bool no_summary = false;
+static bool ipv4_only = false;
+static bool ipv6_only = false;
+static int output_rows = 20;
+static int sort_by = ALL;
+static int interval = 1;
+static int count = 99999999;
+static bool verbose = false;
+
+const char *argp_program_version = "tcptop 0.1";
+const char *argp_program_bug_address =
+	"https://github.com/iovisor/bcc/tree/master/libbpf-tools";
+const char argp_program_doc[] =
+"Trace sending and received operation over IP.\n"
+"\n"
+"USAGE: tcptop [-h] [-p PID] [interval] [count]\n"
+"\n"
+"EXAMPLES:\n"
+"    tcptop            # TCP top, refresh every 1s\n"
+"    tcptop -p 1216    # only trace PID 1216\n"
+"    tcptop -c path    # only trace the given cgroup path\n"
+"    tcptop 5 10       # 5s summaries, 10 times\n";
+
+static const struct argp_option opts[] = {
+	{ "pid", 'p', "PID", 0, "Process ID to trace" },
+	{ "cgroup", 'c', "/sys/fs/cgroup/unified", 0, "Trace process in cgroup path" },
+	{ "ipv4", '4', NULL, 0, "trace IPv4 family only" },
+	{ "ipv6", '6', NULL, 0, "trace IPv6 family only" },
+	{ "nosummary", 'S', NULL, 0, "Skip system summary line"},
+	{ "noclear", 'C', NULL, 0, "Don't clear the screen" },
+	{ "sort", 's', "SORT", 0, "Sort columns, default all [all, sent, received]" },
+	{ "rows", 'r', "R
