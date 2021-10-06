56af7c5d9c7061289dd1c0cdad0102a131d9c1a2
stepsunson <104303353+stepsunson@users.noreply.github.com>
2021-10-06 17:51:22 +0000

    Regenerate gemspec for version 0.0.0

--- scripts/build_bpf_demo.sh
+++ scripts/build_bpf_demo.sh
@@ -32,4 +32,82 @@ while true; do
   case "$1" in
     -b|--bridge) BRIDGE="$2"; shift 2 ;;
     -c|--cpu) CPU="$2"; shift 2 ;;
-    -k|--kickstart) KICKSTART
+    -k|--kickstart) KICKSTART="$2"; shift 2 ;;
+    -n|--name) NAME="$2"; shift 2 ;;
+    -m|--mirror) MIRROR="$2"; shift 2 ;;
+    -M|--mem) MEM="$2"; shift 2 ;;
+    -p|--password) PASSWORD="$2"; shift 2 ;;
+    -s|--size) SIZE="$2"; shift 2 ;;
+    --) shift; break ;;
+    *) usage; exit 1
+      ;;
+  esac
+done
+[[ ! -f "$KICKSTART" ]] && { usage; exit 1; }
+[[ -z "$NAME" ]] && { usage; exit 1; }
+
+PASSWORD=${PASSWORD:-"iovisor"}
+BRIDGE=${BRIDGE:-virbr0}
+MIRROR=${MIRROR:-http://mirror.pnl.gov/fedora/linux/releases/22}
+MEM=${MEM:-4094}
+CPU=${CPU:-4}
+SIZE=${SIZE:-40}
+
+if [[ "$(id -u)" != "0" ]]; then
+  sudo="sudo"
+fi
+
+if ! which virt-install &> /dev/null; then
+  echo "Error: virt-install is not installed"
+  exit 1
+fi
+
+libvirt_dir=/var/lib/libvirt/images
+img_name=$NAME
+tmpdir=$(mktemp -d /tmp/virt-install_XXXXX)
+tmp_ks_file=$tmpdir/$img_name.ks
+
+function cleanup() {
+  set +e
+  [[ -d "$tmpdir" ]] && rm -fr "$tmpdir"
+  local destroy_kvm=n
+  [[ -f "/etc/libvirt/qemu/$img_name.xml" ]] && read -p "Destroy libvirt VM (y/n)? " destroy_kvm
+  if [[ "$destroy_kvm" != n* ]]; then
+    virsh destroy $img_name
+    virsh undefine $img_name
+    virsh vol-delete $img_name.img --pool default
+    $sudo rm -f $libvirt_dir/$img_name.img
+  fi
+}
+trap cleanup EXIT
+
+ruby <<DELIM__
+require 'erb'
+@password="$PASSWORD"
+@name="$NAME"
+@domain="example.com"
+@github_access_token="$GITHUB_ACCESS_TOKEN"
+@mirror="$MIRROR"
+File.open('$tmp_ks_file', 'w') do |f|
+  f.puts ERB.new(File.open('$KICKSTART', 'rb').read, nil, '-').result()
+end
+DELIM__
+
+tree=$MIRROR/Server/x86_64/os/
+virt-install --connect=qemu:///system \
+    --network=bridge:$BRIDGE \
+    --initrd-inject=$tmp_ks_file \
+    --controller type=scsi,model=virtio-scsi \
+    --extra-args="ks=file:/$(basename $tmp_ks_file) console=tty0 console=ttyS0,115200" \
+    --name=$img_name \
+    --disk $libvirt_dir/$img_name.img,cache=none,format=qcow2,size=$SIZE,bus=scsi \
+    --ram $MEM \
+    --vcpus=$CPU \
+    --check-cpu \
+    --accelerate \
+    --hvm \
+    --location=$tree \
+    --nographics
+
+echo "SUCCESS"
+exit 0
---
+++ scripts/c-style-check.sh
@@ -0,0 +1,18 @@
+
+#!/bin/bash
+
+# Runs clang-format on the files changed between HEAD and $1, which defaults to
+# origin/master.
+
+# to pick up git-clang-format from scripts/
+export PATH=$(dirname $0):$PATH
+
+CLANG_FORMAT=${CLANG_FORMAT:-clang-format}
+GITREF=${1:-origin/master}
+
+if ! hash $CLANG_FORMAT 2> /dev/null; then
+  echo "Could not find clang-format tool" 1>&2
+  exit 1
+fi
+
+cmd="git clang-format $GITREF --binary $CLANG_FORMAT --diff --extensions h,c,cc"
