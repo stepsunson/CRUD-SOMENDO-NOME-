2905a3750dac15c6fb3e53d2accc2028ebffd093
stepsunson <104303353+stepsunson@users.noreply.github.com>
2021-11-05 20:32:13 +0000

    Added standard Scrutinizer config

--- src/cc/bcc_proc.h
+++ src/cc/bcc_proc.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 Facebook, Inc.
+ * Copyright (c) 2016 GitHub, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,29 +13,21 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#ifndef LIBBCC_PERF_MAP_H
-#define LIBBCC_PERF_MAP_H
+#ifndef LIBBCC_PROC_H
+#define LIBBCC_PROC_H
+
+#include "bcc_syms.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 #include <stdint.h>
+#include <stdio.h>
 #include <stdbool.h>
-#include <unistd.h>
 
-// Symbol name, start address, length, payload
-typedef int (*bcc_perf_map_symcb)(const char *, uint64_t, uint64_t, void *);
 
-bool bcc_is_perf_map(const char *path);
-bool bcc_is_valid_perf_map(const char *path);
-
-int bcc_perf_map_nstgid(int pid);
-bool bcc_perf_map_path(char *map_path, size_t map_len, int pid);
-int bcc_perf_map_foreach_sym(const char *path, bcc_perf_map_symcb callback,
-                             void* payload);
-
-#ifdef __cplusplus
-}
-#endif
-#endif
+typedef struct mod_info {
+  char *name;
+  uint64_t start_addr;
+  uint64_t end_add
