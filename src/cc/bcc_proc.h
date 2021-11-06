7a2ea8daaaaf884008eeb0b09ad052dc6fc64d5a
stepsunson <104303353+stepsunson@users.noreply.github.com>
2021-11-06 12:54:39 +0000

    Improve some comments

--- src/cc/bcc_proc.h
+++ src/cc/bcc_proc.h
@@ -30,4 +30,39 @@ extern "C" {
 typedef struct mod_info {
   char *name;
   uint64_t start_addr;
-  uint64_t end_add
+  uint64_t end_addr;
+  long long unsigned int file_offset;
+  uint64_t dev_major;
+  uint64_t dev_minor;
+  uint64_t inode;
+} mod_info;
+
+// Module info, whether to check mount namespace, payload
+// Callback returning a negative value indicates to stop the iteration
+typedef int (*bcc_procutils_modulecb)(mod_info *, int, void *);
+
+// Symbol name, address, payload
+typedef void (*bcc_procutils_ksymcb)(const char *, const char *, uint64_t, void *);
+
+char *bcc_procutils_which_so(const char *libname, int pid);
+char *bcc_procutils_which(const char *binpath);
+int bcc_mapping_is_file_backed(const char *mapname);
+// Iterate over all executable memory mapping sections of a Process.
+// All anonymous and non-file-backed mapping sections, namely those
+// listed in bcc_mapping_is_file_backed, will be ignored.
+// Returns -1 on error, and 0 on success
+int bcc_procutils_each_module(int pid, bcc_procutils_modulecb callback,
+                              void *payload);
+
+int _procfs_maps_each_module(FILE *procmaps, int pid,
+                             bcc_procutils_modulecb callback, void *payload);
+// Iterate over all non-data Kernel symbols.
+// Returns -1 on error, and 0 on success
+int bcc_procutils_each_ksym(bcc_procutils_ksymcb callback, void *payload);
+void bcc_procutils_free(const char *ptr);
+const char *bcc_procutils_language(int pid);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
