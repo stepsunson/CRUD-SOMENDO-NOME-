c80ef9f677adbbbe35ec8b9cfd0b87dc52424236
stepsunson <104303353+stepsunson@users.noreply.github.com>
2022-07-01 12:45:05 +0000

    obsolete filter

--- tests/cc/test_hash_table.cc
+++ tests/cc/test_hash_table.cc
@@ -85,4 +85,93 @@ TEST_CASE("test hash table", "[hash_table]") {
       REQUIRE(pair.first / 3 == pair.second);
     }
 
-    // clear tab
+    // clear table
+    t.clear_table_non_atomic();
+    REQUIRE(t.get_table_offline().size() == 0);
+  }
+}
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,6,0)
+TEST_CASE("percpu hash table", "[percpu_hash_table]") {
+  const std::string BPF_PROGRAM = R"(
+    BPF_PERCPU_HASH(myhash, int, u64, 128);
+    BPF_PERCPU_ARRAY(myarray, u64, 64);
+  )";
+
+  ebpf::BPF bpf;
+  ebpf::StatusTuple res(0);
+  res = bpf.init(BPF_PROGRAM);
+  REQUIRE(res.ok());
+
+  ebpf::BPFPercpuHashTable<int, uint64_t> t =
+    bpf.get_percpu_hash_table<int, uint64_t>("myhash");
+  size_t ncpus = ebpf::BPFTable::get_possible_cpu_count();
+
+  SECTION("bad table type") {
+    // try to get table of wrong type
+    auto f1 = [&](){
+      bpf.get_percpu_hash_table<int, uint64_t>("myarray");
+    };
+
+    REQUIRE_THROWS(f1());
+  }
+
+  SECTION("standard methods") {
+    int k;
+    std::vector<uint64_t> v1(ncpus);
+    std::vector<uint64_t> v2;
+
+    for (size_t j = 0; j < ncpus; j++) {
+      v1[j] = 42 * j;
+    }
+
+    k = 1;
+
+    // create new element
+    res = t.update_value(k, v1);
+    REQUIRE(res.ok());
+    res = t.get_value(k, v2);
+    REQUIRE(res.ok());
+    for (size_t j = 0; j < ncpus; j++) {
+      REQUIRE(v2.at(j) == 42 * j);
+    }
+
+    // update existing element
+    for (size_t j = 0; j < ncpus; j++) {
+      v1[j] = 69 * j;
+    }
+    res = t.update_value(k, v1);
+    REQUIRE(res.ok());
+    res = t.get_value(k, v2);
+    REQUIRE(res.ok());
+    for (size_t j = 0; j < ncpus; j++) {
+      REQUIRE(v2.at(j) == 69 * j);
+    }
+
+    // remove existing element
+    res = t.remove_value(k);
+    REQUIRE(res.ok());
+
+    // remove non existing element
+    res = t.remove_value(k);
+    REQUIRE(!res.ok());
+
+    // get non existing element
+    res = t.get_value(k, v2);
+    REQUIRE(!res.ok());
+  }
+
+  SECTION("walk table") {
+    std::vector<uint64_t> v(ncpus);
+
+    for (int k = 3; k <= 30; k+=3) {
+      for (size_t cpu = 0; cpu < ncpus; cpu++) {
+        v[cpu] = k * cpu;
+      }
+      res = t.update_value(k, v);
+      REQUIRE(res.ok());
+    }
+
+    // get whole table
+    auto offline = t.get_table_offline();
+    REQ
