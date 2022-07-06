838d17cb2e6617d511786b0efba5b23fa77ec6b9
stepsunson <104303353+stepsunson@users.noreply.github.com>
2022-07-06 22:15:36 +0000

    update dependencies to latest versions

--- tests/cc/test_map_in_map.cc
+++ tests/cc/test_map_in_map.cc
@@ -209,4 +209,79 @@ TEST_CASE("test hash of maps using custom key", "[hash_of_maps_custom_key]") {
     REQUIRE(value2 == 0);
 
     // Call syscall__getuid, then set value to ex2_table
-    res = cntl_table.update_value
+    res = cntl_table.update_value(key, 2);
+    REQUIRE(res.ok());
+    REQUIRE(getuid() >= 0);
+
+    // Now we can get value from ex2_table
+    res = ex2_table.get_value(key, value2);
+    REQUIRE(res.ok());
+    REQUIRE(value > 0);
+
+    res = bpf.detach_kprobe(getuid_fnname);
+    REQUIRE(res.ok());
+
+    res = t.remove_value(hash_key);
+    REQUIRE(res.ok());
+    res = t.remove_value(hash_key2);
+    REQUIRE(res.ok());
+  }
+}
+
+TEST_CASE("test array of maps", "[array_of_maps]") {
+  {
+    const std::string BPF_PROGRAM = R"(
+      BPF_ARRAY(cntl, int, 1);
+      BPF_TABLE("hash", int, int, ex1, 1024);
+      BPF_TABLE("hash", int, int, ex2, 1024);
+      BPF_TABLE("hash", u64, u64, ex3, 1024);
+      BPF_ARRAY_OF_MAPS(maps_array, "ex1", 10);
+
+      int syscall__getuid(void *ctx) {
+         int key = 0, data, *val, cntl_val;
+         void *inner_map;
+
+         val = cntl.lookup(&key);
+         if (!val || *val == 0)
+           return 0;
+
+         // cntl_val == 1 : lookup and update
+         // cntl_val == 2 : delete
+         cntl_val = *val;
+         inner_map = maps_array.lookup(&key);
+         if (!inner_map)
+           return 0;
+
+         if (cntl_val == 1) {
+           val = bpf_map_lookup_elem(inner_map, &key);
+           if (!val) {
+             data = 1;
+             bpf_map_update_elem(inner_map, &key, &data, 0);
+           }
+         } else if (cntl_val == 2) {
+           bpf_map_delete_elem(inner_map, &key);
+         }
+
+         return 0;
+      }
+    )";
+
+    ebpf::BPF bpf;
+    ebpf::StatusTuple res(0);
+    res = bpf.init(BPF_PROGRAM);
+    REQUIRE(res.ok());
+
+    auto t = bpf.get_map_in_map_table<int>("maps_array");
+    auto ex1_table = bpf.get_hash_table<int, int>("ex1");
+    auto ex2_table = bpf.get_hash_table<int, int>("ex2");
+    auto ex3_table =
+        bpf.get_hash_table<unsigned long long, unsigned long long>("ex3");
+    int ex1_fd = ex1_table.get_fd();
+    int ex2_fd = ex2_table.get_fd();
+    int ex3_fd = ex3_table.get_fd();
+
+    int key = 0, value = 0;
+    res = t.update_value(key, ex1_fd);
+    REQUIRE(res.ok());
+
+    // updating already-occupied slo
