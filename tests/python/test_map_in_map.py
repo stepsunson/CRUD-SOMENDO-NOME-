1c47ac4f4b455d51c04db745bcf0ed57d98ba6b1
stepsunson <104303353+stepsunson@users.noreply.github.com>
2022-08-21 07:10:58 +0000

    drm/radeon: add error output if VM CS fails on cayman

---
+++ tests/python/test_map_in_map.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+#
+# USAGE: test_map_in_map.py
+#
+# Copyright 2019 Facebook, Inc
+# Licensed under the Apache License, Version 2.0 (the "License")
+
+from __future__ import print_function
+from bcc import BPF
+from unittest import main, skipUnless, TestCas
