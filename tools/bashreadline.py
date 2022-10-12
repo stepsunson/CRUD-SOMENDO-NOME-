1f1916be01db5ef2278a573a052d075d8e2783c2
stepsunson <104303353+stepsunson@users.noreply.github.com>
2022-10-12 14:11:33 +0000

    Remove unneeded files

--- tools/bashreadline.py
+++ tools/bashreadline.py
@@ -30,3 +30,53 @@ parser.add_argument("-s", "--shared", nargs="?",
         help="specify the location of libreadline.so library.\
               Default is /lib/libreadline.so")
 args = parser.parse_args()
+
+name = args.shared if args.shared else "/bin/bash"
+
+# load BPF program
+bpf_text = """
+#include <uapi/linux/ptrace.h>
+#include <linux/sched.h>
+
+struct str_t {
+    u32 pid;
+    char str[80];
+};
+
+BPF_PERF_OUTPUT(events);
+
+int printret(struct pt_regs *ctx) {
+    struct str_t data  = {};
+    char comm[TASK_COMM_LEN] = {};
+    if (!PT_REGS_RC(ctx))
+        return 0;
+    data.pid = bpf_get_current_pid_tgid() >> 32;
+    bpf_probe_read_user(&data.str, sizeof(data.str), (void *)PT_REGS_RC(ctx));
+
+    bpf_get_current_comm(&comm, sizeof(comm));
+    if (comm[0] == 'b' && comm[1] == 'a' && comm[2] == 's' && comm[3] == 'h' && comm[4] == 0 ) {
+        events.perf_submit(ctx,&data,sizeof(data));
+    }
+
+
+    return 0;
+};
+"""
+
+b = BPF(text=bpf_text)
+b.attach_uretprobe(name=name, sym="readline", fn_name="printret")
+
+# header
+print("%-9s %-7s %s" % ("TIME", "PID", "COMMAND"))
+
+def print_event(cpu, data, size):
+    event = b["events"].event(data)
+    print("%-9s %-7d %s" % (strftime("%H:%M:%S"), event.pid,
+                            event.str.decode('utf-8', 'replace')))
+
+b["events"].open_perf_buffer(print_event)
+while 1:
+    try:
+        b.perf_buffer_poll()
+    except KeyboardInterrupt:
+        exit()
