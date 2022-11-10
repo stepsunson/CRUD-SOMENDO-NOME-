f5dc07c674da1ed3fe1dc02a606b10e6f803aaad
stepsunson <104303353+stepsunson@users.noreply.github.com>
2022-11-10 17:55:38 +0000

    tweaked css

--- tools/funclatency.py
+++ tools/funclatency.py
@@ -33,4 +33,53 @@ import signal
 examples = """examples:
     ./funclatency do_sys_open       # time the do_sys_open() kernel function
     ./funclatency c:read            # time the read() C library function
-    ./funclatency -u vfs_read       # time vfs_read(), in microse
+    ./funclatency -u vfs_read       # time vfs_read(), in microseconds
+    ./funclatency -m do_nanosleep   # time do_nanosleep(), in milliseconds
+    ./funclatency -i 2 -d 10 c:open # output every 2 seconds, for duration 10s
+    ./funclatency -mTi 5 vfs_read   # output every 5 seconds, with timestamps
+    ./funclatency -p 181 vfs_read   # time process 181 only
+    ./funclatency 'vfs_fstat*'      # time both vfs_fstat() and vfs_fstatat()
+    ./funclatency 'c:*printf'       # time the *printf family of functions
+    ./funclatency -F 'vfs_r*'       # show one histogram per matched function
+"""
+parser = argparse.ArgumentParser(
+    description="Time functions and print latency as a histogram",
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    epilog=examples)
+parser.add_argument("-p", "--pid", type=int,
+    help="trace this PID only")
+parser.add_argument("-i", "--interval", type=int,
+    help="summary interval, in seconds")
+parser.add_argument("-d", "--duration", type=int,
+    help="total duration of trace, in seconds")
+parser.add_argument("-T", "--timestamp", action="store_true",
+    help="include timestamp on output")
+parser.add_argument("-u", "--microseconds", action="store_true",
+    help="microsecond histogram")
+parser.add_argument("-m", "--milliseconds", action="store_true",
+    help="millisecond histogram")
+parser.add_argument("-F", "--function", action="store_true",
+    help="show a separate histogram per function")
+parser.add_argument("-r", "--regexp", action="store_true",
+    help="use regular expressions. Default is \"*\" wildcards only.")
+parser.add_argument("-l", "--level", type=int,
+    help="set the level of nested or recursive functions")
+parser.add_argument("-v", "--verbose", action="store_true",
+    help="print the BPF program (for debugging purposes)")
+parser.add_argument("pattern",
+    help="search expression for functions")
+parser.add_argument("--ebpf", action="store_true",
+    help=argparse.SUPPRESS)
+args = parser.parse_args()
+if args.duration and not args.interval:
+    args.interval = args.duration
+if not args.interval:
+    args.interval = 99999999
+
+def bail(error):
+    print("Error: " + error)
+    exit(1)
+
+parts = args.pattern.split(':')
+if len(parts) == 1:
+    lib
