65a48270c74562ac65d69949f8b116d4cf856457
stepsunson <104303353+stepsunson@users.noreply.github.com>
2023-01-29 10:50:24 +0000

    Oops..

--- tools/tcpretrans.py
+++ tools/tcpretrans.py
@@ -28,4 +28,89 @@ examples = """examples:
     ./tcpretrans           # trace TCP retransmits
     ./tcpretrans -l        # include TLP attempts
     ./tcpretrans -4        # trace IPv4 family only
-    ./tcpr
+    ./tcpretrans -6        # trace IPv6 family only
+"""
+parser = argparse.ArgumentParser(
+    description="Trace TCP retransmits",
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    epilog=examples)
+parser.add_argument("-s", "--sequence", action="store_true",
+    help="display TCP sequence numbers")
+parser.add_argument("-l", "--lossprobe", action="store_true",
+    help="include tail loss probe attempts")
+parser.add_argument("-c", "--count", action="store_true",
+    help="count occurred retransmits per flow")
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-4", "--ipv4", action="store_true",
+    help="trace IPv4 family only")
+group.add_argument("-6", "--ipv6", action="store_true",
+    help="trace IPv6 family only")
+parser.add_argument("--ebpf", action="store_true",
+    help=argparse.SUPPRESS)
+args = parser.parse_args()
+debug = 0
+
+# define BPF program
+bpf_text = """
+#include <uapi/linux/ptrace.h>
+#include <net/sock.h>
+#include <net/tcp.h>
+#include <bcc/proto.h>
+
+#define RETRANSMIT  1
+#define TLP         2
+
+// separate data structs for ipv4 and ipv6
+struct ipv4_data_t {
+    u32 pid;
+    u64 ip;
+    u32 seq;
+    u32 saddr;
+    u32 daddr;
+    u16 lport;
+    u16 dport;
+    u64 state;
+    u64 type;
+};
+BPF_PERF_OUTPUT(ipv4_events);
+
+struct ipv6_data_t {
+    u32 pid;
+    u32 seq;
+    u64 ip;
+    unsigned __int128 saddr;
+    unsigned __int128 daddr;
+    u16 lport;
+    u16 dport;
+    u64 state;
+    u64 type;
+};
+BPF_PERF_OUTPUT(ipv6_events);
+
+// separate flow keys per address family
+struct ipv4_flow_key_t {
+    u32 saddr;
+    u32 daddr;
+    u16 lport;
+    u16 dport;
+};
+BPF_HASH(ipv4_count, struct ipv4_flow_key_t);
+
+struct ipv6_flow_key_t {
+    unsigned __int128 saddr;
+    unsigned __int128 daddr;
+    u16 lport;
+    u16 dport;
+};
+BPF_HASH(ipv6_count, struct ipv6_flow_key_t);
+"""
+
+bpf_text_kprobe = """
+static int trace_event(struct pt_regs *ctx, struct sock *skp, struct sk_buff *skb, int type)
+{
+    struct tcp_skb_cb *tcb;
+    u32 seq;
+
+    if (skp == NULL)
+        return 0;
+    u32 pid = bpf_get_current_pid_tgid(
