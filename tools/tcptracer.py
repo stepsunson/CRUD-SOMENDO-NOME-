d6bc10f850644348ae548ddb658be43a5e160f9c
stepsunson <104303353+stepsunson@users.noreply.github.com>
2023-02-06 17:39:08 +0000

    2004-04-30  Andrew Cagney  <cagney@redhat.com>

--- tools/tcptracer.py
+++ tools/tcptracer.py
@@ -40,4 +40,97 @@ group.add_argument("-4", "--ipv4", action="store_true",
 group.add_argument("-6", "--ipv6", action="store_true",
                    help="trace IPv6 family only")
 parser.add_argument("-v", "--verbose", action="store_true",
-                    help="include Network Namespace in the o
+                    help="include Network Namespace in the output")
+parser.add_argument("--ebpf", action="store_true",
+                    help=ap.SUPPRESS)
+args = parser.parse_args()
+
+bpf_text = """
+#include <uapi/linux/ptrace.h>
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wtautological-compare"
+#include <net/sock.h>
+#pragma clang diagnostic pop
+#include <net/inet_sock.h>
+#include <net/net_namespace.h>
+#include <bcc/proto.h>
+
+#define TCP_EVENT_TYPE_CONNECT 1
+#define TCP_EVENT_TYPE_ACCEPT  2
+#define TCP_EVENT_TYPE_CLOSE   3
+
+struct tcp_ipv4_event_t {
+    u64 ts_ns;
+    u32 type;
+    u32 pid;
+    char comm[TASK_COMM_LEN];
+    u8 ip;
+    u32 saddr;
+    u32 daddr;
+    u16 sport;
+    u16 dport;
+    u32 netns;
+};
+BPF_PERF_OUTPUT(tcp_ipv4_event);
+
+struct tcp_ipv6_event_t {
+    u64 ts_ns;
+    u32 type;
+    u32 pid;
+    char comm[TASK_COMM_LEN];
+    unsigned __int128 saddr;
+    unsigned __int128 daddr;
+    u16 sport;
+    u16 dport;
+    u32 netns;
+    u8 ip;
+};
+BPF_PERF_OUTPUT(tcp_ipv6_event);
+
+// tcp_set_state doesn't run in the context of the process that initiated the
+// connection so we need to store a map TUPLE -> PID to send the right PID on
+// the event
+struct ipv4_tuple_t {
+    u32 saddr;
+    u32 daddr;
+    u16 sport;
+    u16 dport;
+    u32 netns;
+};
+
+struct ipv6_tuple_t {
+    unsigned __int128 saddr;
+    unsigned __int128 daddr;
+    u16 sport;
+    u16 dport;
+    u32 netns;
+};
+
+struct pid_comm_t {
+    u64 pid;
+    char comm[TASK_COMM_LEN];
+};
+
+BPF_HASH(tuplepid_ipv4, struct ipv4_tuple_t, struct pid_comm_t);
+BPF_HASH(tuplepid_ipv6, struct ipv6_tuple_t, struct pid_comm_t);
+
+BPF_HASH(connectsock, u64, struct sock *);
+
+static int read_ipv4_tuple(struct ipv4_tuple_t *tuple, struct sock *skp)
+{
+  u32 net_ns_inum = 0;
+  u32 saddr = skp->__sk_common.skc_rcv_saddr;
+  u32 daddr = skp->__sk_common.skc_daddr;
+  struct inet_sock *sockp = (struct inet_sock *)skp;
+  u16 sport = sockp->inet_sport;
+  u16 dport = skp->__sk_common.skc_dport;
+#ifdef CONFIG_NET_NS
+  net_ns_inum = skp->__sk_common.skc_net.net->ns.inum;
+#endif
+
+  ##FILTER_NETNS##
+
+  tuple->saddr = saddr;
+  tuple->daddr = daddr;
+  tuple->sport = sport;
+  tuple->d
